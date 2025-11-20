c8c787317b46af603b44b62c2f07829839265f29
Dylan Grousson <grousson.dylan@gmail.com>
2025-11-20 10:08:17 +0100

    removed config and target files

--- game/src/main/java/GhostHunter.java
+++ game/src/main/java/GhostHunter.java
@@ -1,5 +1,15 @@
 public class GhostHunter {
-    public static void main(String [] args) {
-        System.out.println("Hello");
+    public static void main(String[] args) {
+        int n = 5;
+        Boolean[][] adj = {{false, true, true, true, true},
+                           {true, false, false, false, false},
+                           {true, false, false, true, false},
+                           {true, false, true, false, true},
+                           {true, false, false, true, false}};
+        Graph g = new Graph(n, adj);
+        System.out.println(g);
+        for (int i = 0; i < n; i++) {
+            System.out.println(g.getNeighborList(i));
+        }
     }
 }
---
+++ game/src/main/java/Graph.java
@@ -0,0 +1,56 @@
+import java.util.ArrayList;
+
+public class Graph {
+    private int n_;
+    private Boolean[][] edges_;
+    private ArrayList<Integer>[] neighbors_;
+
+    public Graph(int nbNode, Boolean[][] adjacencyMatrix) {
+        n_ = nbNode;
+        edges_ = adjacencyMatrix;
+
+        neighbors_ =
+            (ArrayList<Integer>[]) new ArrayList[n_]; // Cast to
+                                                      // ArrayList<Integer>[]
+
+        for (int i = 0; i < n_; i++) {
+            neighbors_[i] = new ArrayList<Integer>();
+        }
+        for (int i = 0; i < n_ - 1; i++) {
+            for (int j = i + 1; j < n_; j++) {
+                if (edges_[i][j] == true) {
+                    neighbors_[i].add(j);
+                    neighbors_[j].add(i);
+                }
+            }
+        }
+    }
+
+    int getN() { return n_; }
+
+    Boolean[][] getEdges() { return edges_; }
+
+    ArrayList<Integer>[] getNeighbors() { return neighbors_; }
+
+    ArrayList<Integer> getNeighborList(int vertex) {
+        return neighbors_[vertex];
+    }
+
+    public String toString() {
+        String str = "|V| = " + n_ + " and E =\n";
+
+        for (int i = 0; i < n_; i++) {
+            str += "[";
+            for (int j = 0; j < n_; j++) {
+                if (edges_[i][j]) {
+                    str += "1 ";
+                } else {
+                    str += "0 ";
+                }
+            }
+            str = str.substring(0, str.length() - 1) + "]\n";
+        }
+
+        return str;
+    }
+}
